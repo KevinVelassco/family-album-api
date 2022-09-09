60c285457c28508c51ce67d17a40187b17d21ddc
KevinVelassco <kevinvelassco@gmail.com>
2022-09-09 12:15:50 -0500

    feat: the initial configuration of the required environment variables is performed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import appConfig from './config/app.config';
+import appConfigSchema from './config/app.schema.config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      validationSchema: appConfigSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/app.config.ts
@@ -0,0 +1,17 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => ({
+  environment: process.env.NODE_ENV || 'development',
+  app: {
+    port: parseInt(process.env.PORT, 10) || 8080,
+  },
+  database: {
+    client: process.env.DATABASE_CLIENT,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    log: process.env.DATABASE_LOG || 'yes',
+  },
+}));
