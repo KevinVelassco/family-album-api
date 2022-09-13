0a7006f763d893f245078b3a9266e76d58d1b83b
KevinVelassco <kevinvelassco@gmail.com>
2022-09-13 11:22:31 -0500

    feat: the common module is added and the custom exception filter is created

---
+++ src/common/common.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { CustomExceptionFilter } from './filters/custom-exception.filter';
+
+@Module({
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: CustomExceptionFilter,
+    },
+  ],
+})
+export class CommonModule {}
