653b10d6f6ab0bc0fbb09ea108243c32b6557ad5
KevinVelassco <kevinvelassco@gmail.com>
2022-09-13 10:56:20 -0500

    feat: initial endpoint from swagger documentation is excluded

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiExcludeEndpoint()
   @Get()
   @Render('index')
   render() {
