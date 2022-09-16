7b2df2b5850511ef0da274b7f6841805ba74922f
KevinVelassco <kevinvelassco@gmail.com>
2022-09-16 16:23:17 -0500

    feat: dto is created (update user)

--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -1,3 +1,4 @@
 export { CreateUserDto } from './create-user.dto';
 export { FindAllUsersDto } from './find-all-users.dto';
 export { FindOneUserDto } from './find-one-user.dto';
+export { UpdateUserDto } from './update-user.dto';
