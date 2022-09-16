b6f87246760509da05d86136b0726706ef1647f8
KevinVelassco <kevinvelassco@gmail.com>
2022-09-16 15:21:07 -0500

    feat: the route to find one user is added to the controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,22 +1,29 @@
-import { Controller, Post, Body, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query, Param } from '@nestjs/common';
 import {
+  ApiBadRequestResponse,
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
 
 import { UserService } from './user.service';
 import { User } from './user.entity';
-import { CreateUserDto, FindAllUsersDto } from './dto';
 import { ResultsOutputDto } from '../../common/dto';
 import { ApiResultsResponse } from '../../common/decorators';
+import { CreateUserDto, FindAllUsersDto, FindOneUserDto } from './dto';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @ApiCreatedResponse({ description: 'User was created.', type: User })
+  @ApiCreatedResponse({
+    description: 'The user has been successfully created.',
+    type: User,
+  })
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
   @ApiConflictResponse({
     description:
       'User with email already exists. | User with phone already exists.',
@@ -27,6 +34,7 @@ export class UserController {
   }
 
   @ApiResultsResponse(User)
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
   @ApiConflictResponse({
     description: 'Limit greater than 50.',
   })
@@ -35,5 +43,13 @@ export class UserController {
     @Query() findAllUsersDto: FindAllUsersDto,
   ): Promise<ResultsOutputDto<User>> {
     return this.userService.findAll(findAllUsersDto);
+  }
+
+  @ApiOkResponse({ type: User })
+  @ApiBadRequestResponse({ description: 'authUid must be a UUID.' })
+  @ApiNotFoundResponse({ description: 'user not found.' })
+  @Get(':authUid')
+  findOne(@Param() findOneUserDto: FindOneUserDto): Promise<User | null> {
+    return this.userService.findOne(findOneUserDto);
   }
 }
