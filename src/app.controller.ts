29c6f6a30f4524cc3eec1130806756da79c79eee
KevinVelassco <kevinvelassco@gmail.com>
2022-09-23 14:50:53 -0500

    feat: the parent route is set to public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { Public } from './common/decorators';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @ApiExcludeEndpoint()
+  @Public()
   @Get()
   @Render('index')
   render() {
